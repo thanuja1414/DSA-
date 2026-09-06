b5ea50676527fe9a39d3ecb7a6eb0a3ede213fec
thanuja1414 <thanujagatakala1414@gmail.com>
2026-09-06 22:20:20 +0530

    datatype modifiers

--- Day-6/2-bitwiseOperators.cpp
+++ Day-6/2-bitwiseOperators.cpp
@@ -12,5 +12,22 @@ int main(){
 
     //operator precedence
     cout<<4*5%2<<endl; 
+  
+
+    //datatype modifiers
+    cout<<"datatype modifiers"<<endl;
+    cout<<sizeof(int)<<endl;
+    cout<<sizeof(long int)<<endl;
+    cout<<sizeof(long)<<endl;
+    cout<<sizeof(short)<<endl;
+    cout<<sizeof(short int)<<endl;
+    cout<<sizeof(long long)<<endl; //implicitly 'long long' means 'long long int'
+
+    unsigned int customerID = 24;
+    cout<<customerID<<endl;
+
+    unsigned int x =-10; // here 2's complement(binary form) of -10 is stored but when asked to print , just the decimal form of binary number is printed.(not -10 , another number whose MSD is actually 1 not the 1 that shows (magnitude) , if the number is +ve or -ve)
+    cout<<x<<endl;
+
     return 0;
 }
