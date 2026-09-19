51a391b5c9bcc68cf46fd35294287e5e12308d58
thanuja1414 <thanujagatakala1414@gmail.com>
2026-09-19 19:48:32 +0530

    peak index in mountain array modified

--- Day-16/2-optimized.cpp
+++ Day-16/2-optimized.cpp
@@ -8,7 +8,7 @@ using namespace std;
 int peakIndex(vector<int>arr){
     int start = 0;
     int end = arr.size()-1;
-    while(start<=end){
+    while(start<end){
         int mid = start + ((end-start)/2);
         if(arr[mid]<arr[mid+1]){
             //increasing slope
